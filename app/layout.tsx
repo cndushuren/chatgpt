d5045cdd9f7f861327bccbab1aea19da6afa3335
liz <931462664@qq.com>
2023-04-16 21:44:34 +0800

    website rename

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "chatgpt",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "chatgpt",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
